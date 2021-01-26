24bad55ae2a27058bf54c52a01729487d8c266a1
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-26 13:07:56 -0300

    evitando erros

--- main.py
+++ main.py
@@ -10,70 +10,72 @@ class CompileRepo:
         with open('repositories.txt', 'r') as file:
             self.repos = file.read().split()
 
+
+
         for repo in self.repos:
-            print(f"Pesuisando o repositório '{repo.split('/')[1]}' de {repo.split('/')[0]}")
-            # instancia o obj Tree
-            tree = treelib.Tree()
-            tree.create_node(f"[{repo}]", '/')
-            for item in get_data_repository_full(repo):
-                if item['type_file'] == 'Directory':
+            try:
+                print(f"Pesuisando o repositório '{repo.split('/')[1]}' de {repo.split('/')[0]}")
+                # instancia o obj Tree
+                tree = treelib.Tree()
+                tree.create_node(f"[{repo}]", '/')
+                for item in get_data_repository_full(repo):
+                    if item['type_file'] == 'Directory':
 
-                    name = f"[{item['name']}]"
+                        name = f"[{item['name']}]"
 
-                    parent = item['url'].replace(f'/{repo}/tree/master', '') \
-                        .replace(item['name'].replace(' ', '%20'), '')
+                        parent = item['url'].replace(f'/{repo}/tree/master', '') \
+                            .replace(item['name'].replace(' ', '%20'), '')
 
-                    path_name = item['url'].replace(f'/{repo}/tree/master', '')
+                        path_name = item['url'].replace(f'/{repo}/tree/master', '')
 
-                    # Verifica se não é o path base '/'
-                    if len(parent) > 1:
-                        # remove o ultimo elemento que é a barra
-                        parent = parent[:-1]
-
-                    # print(path_name)
-                    # print(name)
-                    # print('parent:' + parent)
-                    # print(item)
-
-                    try:
-                        tree.create_node(name, path_name, parent=parent)
-                    except Exception as error:
-                        pass
-                        # print()
-                        # print(parent.split('/')[1:])
-                        # print(parent.split('/')[-1])
-                        # print(error)
-                    # print()
-                # se o item coletado for do tipo File
-
-            for item in get_data_repository_full(repo):
-                if item['type_file'] == 'File':
-                    # busca os itens
-                    lb = lines_bytes(item, repo, self)
-                    if lb:
-                        parent = f"{lb['url'].replace(f'/{repo}/blob/master', '').replace(item['name'], '')}"
-                        formated_nome = f"{lb['name']} ({lb['lb']['lines']} linhas)"
-
+                        # Verifica se não é o path base '/'
                         if len(parent) > 1:
                             # remove o ultimo elemento que é a barra
                             parent = parent[:-1]
 
-                        # print(parent)
-                        # print(formated_nome)
-                        # print(lb)
+                        # print(path_name)
+                        # print(name)
+                        # print('parent:' + parent)
+                        # print(item)
 
                         try:
-                            tree.create_node(formated_nome, formated_nome, parent=parent)
+                            tree.create_node(name, path_name, parent=parent)
                         except Exception as error:
                             pass
+                            # print()
+                            # print(parent.split('/')[1:])
+                            # print(parent.split('/')[-1])
                             # print(error)
                         # print()
-            tree.show()
-            # analisar -> Fazer Tree partindo  dos caminhos dos arquivos, pois o git-hub não sobe pastas vazias
-            print('#' * 100, '\n')
+                    # se o item coletado for do tipo File
 
-            # break
+                for item in get_data_repository_full(repo):
+                    if item['type_file'] == 'File':
+                        # busca os itens
+                        lb = lines_bytes(item, repo, self)
+                        if lb:
+                            parent = f"{lb['url'].replace(f'/{repo}/blob/master', '').replace(item['name'], '')}"
+                            formated_nome = f"{lb['name']} ({lb['lb']['lines']} linhas)"
 
+                            if len(parent) > 1:
+                                # remove o ultimo elemento que é a barra
+                                parent = parent[:-1]
+
+                            # print(parent)
+                            # print(formated_nome)
+                            # print(lb)
+
+                            try:
+                                tree.create_node(formated_nome, formated_nome, parent=parent)
+                            except Exception as error:
+                                pass
+                                # print(error)
+                            # print()
+                tree.show()
+                # analisar -> Fazer Tree partindo  dos caminhos dos arquivos, pois o git-hub não sobe pastas vazias
+                print('#' * 100, '\n')
+            except:
+                pass
 
 if __name__ == '__main__':
     CompileRepo()
