2b6d4c9bfa8e2f45d947f22c663da7b13ff4da78
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-27 09:56:29 -0300

    criando método principal coleta os repositorios do arquivo 'repositories.txt'

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 class CompileRepo:
     def __init__(self):
-        pass
+        with open('repositories.txt', 'r') as file:
+            self.repos = file.read().split()
+
+        print(self.repos)
+
+
+if __name__ == '__main__':
+    CompileRepo()
