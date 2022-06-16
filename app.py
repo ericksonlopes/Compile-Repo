8a654c9ad86928c860ad1e10d33e7bc378f7939b
Erickson-lopes <ofc.erickson@gmail>
2022-06-16 20:04:41 -0300

    FEAT: Capturando dados das branchs (nova rota)

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import json
-
 from flask import Flask, jsonify, request, redirect
 
-from compile_repo import CompileRepo
+from compile_repo import CompileRepo, BranchsRepo
 
 app = Flask('__name__')
 
@@ -12,11 +10,11 @@ def redireciona() -> jsonify:
     return redirect('https://github.com/Erickson-lopes-dev/Compile-Repo', code=302)
 
 
-@app.route('/repo', methods=['GET'])
+@app.route('/repo', methods=['POST'])
 def repo() -> jsonify:
     try:
         # Armazena a data no json
-        json_data = json.loads(request.data)
+        json_data = request.get_json()
 
         # Verifica se o field existe no json
         if 'repository' not in json_data.keys():
@@ -24,6 +22,11 @@ def repo() -> jsonify:
 
         # Instância da classe
         cr = CompileRepo(json_data['repository'])
+
+        if 'branch' in json_data.keys():
+            cr.branch = json_data['branch']
+
+        cr.get_diretories_and_files()
 
         # recebe o objeto com todos os dados
         data_full = cr.return_full_data_repo()
@@ -33,6 +36,24 @@ def repo() -> jsonify:
 
     else:
         return jsonify(data_full), 200
+
+
+@app.route('/get-branches', methods=['POST'])
+def get_branchs() -> jsonify:
+    try:
+        # Armazena a data no json
+        json_data = request.get_json()
+
+        # Verifica se o field existe no json
+        if 'repository' not in json_data.keys():
+            return jsonify({'erro': f"Field 'repository' not found"}), 400
+
+        branchs = BranchsRepo(json_data['repository']).get_all_branchs()
+
+    except Exception as error:
+        return jsonify({'erro': f"{str(error)} {str(type(error))}"}), 400
+    else:
+        return jsonify(branchs), 200
 
 
 if __name__ == '__main__':
--- compile_repo.py
+++ compile_repo.py
@@ -1,9 +1,9 @@
-from typing import List
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
 
-from utils import DirectoryModel, FileModel, FullDataRepo
+from utils import *
 
 
 class CompileRepo:
@@ -12,9 +12,6 @@ class CompileRepo:
         self.__files_list: List[FileModel] = []
         self.__directory_list: List[DirectoryModel] = []
         self.__branch: str = ''
-
-        # Executa funções que traz os dados
-        self.__get_diretories_and_files()
 
     @property
     def repository(self) -> str:
@@ -32,8 +29,12 @@ class CompileRepo:
     def branch(self):
         return self.__branch
 
+    @branch.setter
+    def branch(self, branch):
+        self.__branch = branch
+
     @classmethod
-    def __html_convert_bs4(cls, url) -> BeautifulSoup:
+    def html_convert_bs4(cls, url) -> BeautifulSoup:
         """
         Função Converte o conteudo HTML recebido pela requisição em objetos python, Retornando esse obj
         :param url: url completa
@@ -44,7 +45,7 @@ class CompileRepo:
         # beautifulSoup transforma um documento HTML complexo em uma árvore complexa de objetos Python.
         return BeautifulSoup(req_get.content, 'html.parser')  # retornando o obj
 
-    def __get_diretories_and_files(self) -> None:
+    def get_diretories_and_files(self) -> None:
         """
         Captura todos os dados necessarios (diretorios e arquivos)
         :return:
@@ -54,10 +55,11 @@ class CompileRepo:
         while len(directorys) > 0:
             for link in directorys:
                 sub_directorys = []
-                req = self.__html_convert_bs4(link)
+                req = self.html_convert_bs4(link)
 
                 # Captura o nome da branch
-                self.__branch = req.find(class_='btn css-truncate').text.strip('\n')
+                if self.__branch == '':
+                    self.__branch = req.find(class_='btn css-truncate').text.strip('\n')
 
                 # Frid que localiza todos os arquivos
                 grid_itens = req.find(class_='js-details-container Details')
@@ -114,7 +116,7 @@ class CompileRepo:
         replace_link: str = file.link.replace('blob', 'blame')
 
         # Recebe o objeto bs4 para scraping
-        req_file: BeautifulSoup = self.__html_convert_bs4(replace_link)
+        req_file: BeautifulSoup = self.html_convert_bs4(replace_link)
 
         # Recebe o texto
         file_info: str = req_file.find(class_='file-info').text
@@ -128,14 +130,66 @@ class CompileRepo:
 
         return file
 
-    def return_full_data_repo(self) -> FullDataRepo:
+    def return_full_data_repo(self) -> FullDataModel:
         """
         Retorna todos os dados coletados em um objeto
         :return: FullDataRepo
         """
-        return FullDataRepo(
+        return FullDataModel(
             repository=self.__repository,
             files=self.__files_list,
             directories=self.__directory_list,
             branch=self.__branch
         )
+
+
+class BranchsRepo(CompileRepo):
+    def __init__(self, repository: str):
+        super().__init__(repository)
+        self.__branches_all_url = self.repository + '/branches/all'
+        self.__branchs = []
+
+    @property
+    def branchs(self):
+        return self.__branchs
+
+    def get_all_branchs(self) -> List[BranchModel]:
+        num = 1
+
+        while num:
+            soup_html = self.html_convert_bs4(self.__branches_all_url + f'?page={num}')
+
+            # Pega todas as classes de cada linha com a branch
+            li_list: List[BeautifulSoup] = soup_html.find_all(class_='Box-row position-relative')
+
+            # se não tiver nenhum item, cancela o loop
+            if not li_list:
+                break
+
+            for li in li_list:
+                # linha da branch
+                branch_filter_item = li.find('branch-filter-item')
+                # Pega o nome da branch
+                name = branch_filter_item.attrs['branch']
+                # pega o caminho da branch
+                path = branch_filter_item.attrs['dialog-body-path']
+
+                # Captura as divs da linha da branch
+                updated = branch_filter_item.findAll('div')
+                # Pera o primeiro item que contém a data e quem commitou
+                updated = updated[0].text
+                # Retira os espaços e o nome da branch
+                updated = updated.replace(name, '')
+                # Separa as letras pelos espaços
+                updated = updated.split()
+
+                # Pega o nome de quem fez o commit
+                updated_by = updated[-1]
+                # Pega a data da atualização
+                updated_date = datetime.strptime(' '.join(updated[1:4]), '%b %d, %Y').date()
+
+                self.__branchs.append(BranchModel(name=name, path=path, update_by=updated_by, update_date=updated_date))
+
+            num += 1
+
+        return self.__branchs
